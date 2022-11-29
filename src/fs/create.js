77c446029601f0568a6904723516b6964a340190
Andrei <andrei.bobryk@celadonsoft.com>
2022-11-29 23:24:15 +0300

    feat: implement create.js task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import fs from 'fs/promises'
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+    const path = './src/fs/files/fresh.txt'
+        const content = 'I am fresh and young'
+
+        try {
+            await fs.writeFile(path, content, {
+                encoding: "utf8",
+                flag: "wx",
+                mode: 0o666
+            })
+        } catch (e) {
+            throw new Error('FS operation failed')
+        }
 };
+
+// if ((await fs.stat('./files/fresh.txt')).isFile()) {
+//     console.log('yet')
+//     throw new Error('FS operation failed')
+// } else {
+//     const content = 'I am fresh and young'
+//     await fs.writeFile('./files/fresh.txt', content)
+// }
 
 await create();
