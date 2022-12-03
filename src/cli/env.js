2b7c0f095e657390511b6d873915b3e11d857209
Andrei <andrei.bobryk@celadonsoft.com>
2022-12-03 20:21:19 +0300

    complete tasks cli and hash

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'RSS_'
+    const needEnvVariablesKeys = Object.keys(process.env).filter(key => key.includes(prefix))
+    needEnvVariablesKeys.forEach(key => {
+        const output = key + '=' + process.env[key]
+        console.log(output)
+    })
 };
 
 parseEnv();
--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import  { createReadStream }  from 'node:fs'
+import { argv } from 'node:process'
+const {
+    createHash
+} = await import('crypto');
+import path, {dirname} from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const nameFile = 'fileToCalculateHashFor.txt'
+    const __dirname = dirname(argv[1])
+    const filename = path.join(__dirname, 'files', nameFile)
+
+    const hash = await createHash('sha256')
+    const input = createReadStream(filename)
+    input.on('readable', ()=>{
+        const data = input.read();
+        if (data) {
+          hash.update(data)
+        } else {
+            console.log(`${nameFile} hash equal: ${hash.digest('hex')}`)
+
+        }
+    })
 };
 
 await calculateHash();
