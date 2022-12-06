8945ec134a8c03254b022068403a2a67b98c2ea2
Andrei <andrei.bobryk@celadonsoft.com>
2022-12-07 00:00:37 +0300

    fix: path in fs:copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -2,7 +2,7 @@ import fs from 'fs/promises'
 const copy = async () => {
     try {
         const source = './src/fs/files'
-        const destination = './src/fs/files'
+        const destination = './src/fs/files_copy'
         await fs.cp(source, destination, {errorOnExist: true, force: false, recursive: true})
     } catch (e) {
         throw new Error('FS operation failed')
